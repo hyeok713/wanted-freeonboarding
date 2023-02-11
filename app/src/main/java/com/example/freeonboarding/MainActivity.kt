62247ce2a36e4a295bd9fd34efd3215e73a1e875
HYEOKBEOM <part1shb@gmail.com>
2023-02-11 17:27:18 +0900

    원티드 프리 온 보딩

---
+++ app/src/main/java/com/example/freeonboarding/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.freeonboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.test.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.constraintlayout)
+    }
+}
