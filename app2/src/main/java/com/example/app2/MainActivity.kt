4d26653a63243798eb725894ea5560a4baf6351f
HYEOKBEOM <part1shb@gmail.com>
2023-02-12 12:52:15 +0900

    Jetpack Navigation responsive UIs

---
+++ app2/src/main/java/com/example/app2/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.app2
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.app2.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.navigationrail.NavigationRailView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: Any = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        when (navView) {
+            is BottomNavigationView -> navView.setupWithNavController(navController)
+            is NavigationRailView -> navView.setupWithNavController(navController)
+            is NavigationView -> navView.setupWithNavController(navController)
+        }
+    }
+}
